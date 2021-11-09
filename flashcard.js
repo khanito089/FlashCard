5b18b96fd3ce79f8e2d8f85e912ac8a84cf449a2
khanito089 <khanito089@gmail.com>
2021-11-09 03:14:05 -0500

    flip card added

--- flashcard.js
+++ flashcard.js
@@ -1,1 +1,119 @@
-const gameBoard = document.getElementsByClassName('box')
+const cardsArray = [
+	{
+		name: 'Ace',
+		img: 'img/Ace.png',
+	},
+	{
+		name: 'King',
+		img: 'img/king.png',
+	},
+	{
+		name: 'Queen',
+		img: 'img/queen.png',
+	},
+	{
+		name: 'Joker',
+		img: 'img/joker.png',
+	},
+	{
+		name: 'Ten',
+		img: 'img/ten.png',
+	},
+    {
+		name: 'Nine',
+		img: 'img/nine.png',
+	},
+	{
+		name: 'Eight',
+		img: 'img/eight.png',
+	},
+	{
+		name: 'Seven',
+		img: 'img/seven.png',
+	},
+	{
+		name: 'Six',
+		img: 'img/six.png',
+	},
+	{
+		name: 'Five',
+		img: 'img/five.png',
+	},
+    {
+		name: 'Ace',
+		img: 'img/Ace.png',
+	},
+	{
+		name: 'King',
+		img: 'img/king.png',
+	},
+	{
+		name: 'Queen',
+		img: 'img/queen.png',
+	},
+	{
+		name: 'Joker',
+		img: 'img/joker.png',
+	},
+	{
+		name: 'Ten',
+		img: 'img/ten.png',
+	},
+    {
+		name: 'Nine',
+		img: 'img/nine.png',
+	},
+	{
+		name: 'Eight',
+		img: 'img/eight.png',
+	},
+	{
+		name: 'Seven',
+		img: 'img/seven.png',
+	},
+	{
+		name: 'Six',
+		img: 'img/six.png',
+	},
+	{
+		name: 'Five',
+		img: 'img/five.png',
+	},
+]
+
+const gameBoard = document.querySelector('.grid')
+const playAgain = document.querySelector('button')
+
+let cardSelected = []
+let cardImg = []
+let click = 0
+
+
+function startGame() {
+
+    cardsArray.sort(function () {
+	return 0.5 - Math.random()
+})
+	for (let i = 0; i < cardsArray.length; i++) {
+		let card = document.createElement('img')
+		card.setAttribute('src', 'img/backOfCard.png')
+		card.setAttribute('data-id', i)
+		card.addEventListener('click', flipCard)
+        gameBoard.appendChild(card)
+	}
+}
+
+function flipCard(event) {
+	let cardType = event.target.getAttribute('data-id')
+	cardSelected.push(cardsArray[cardType].name)
+	cardImg.push(cardType)
+	event.target.setAttribute('src', cardsArray[cardType].img)
+	click++
+}
+    startGame()
+
+playAgain.addEventListener('click', restartGame)
+function restartGame() {
+    gameBoard.innerHTML = ''
+    startGame()
+}
